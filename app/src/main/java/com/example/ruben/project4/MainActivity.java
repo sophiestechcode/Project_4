ecc66797e0592e28c8a0c728c7e8d5b862be1a99
0872303 <0872303@hr.nl>
2016-07-02 17:19:01 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/ruben/project4/MainActivity.java
+++ app/src/main/java/com/example/ruben/project4/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.ruben.project4;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import java.util.List;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -87,13 +92,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(knop);
             }
         });
+
+        DBHandler1 db1 = new DBHandler1(this);
+
+        // Reading M1
+        Log.d("Reading: ", "Reading M1..");
+        List<M1> m1s = db1.getAllM1();
+
+        for (M1 m1 : m1s) {
+            String log = "Omschrijving: " + m1.getOmschrijving() + " ,Deelgemeente: " + m1.getDeelgemeente();
+            // Writing shops to log
+            Log.d("M1: : ", log);
+        }
+
+        DBHandler2 db2 = new DBHandler2(this);
+
+        // Reading M2
+        Log.d("Reading: ", "Reading M2..");
+        List<M2> m2s = db2.getAllM2();
+
+        for (M2 m2 : m2s) {
+            String log = "MK_omschrijving: " + m2.getMK_omschrijving() + " ,Gemiddelde_maand: " + m2.getGemiddelde_maand();
+            // Writing shops to log
+            Log.d("M2: : ", log);
+        }
+
+        DBHandler3 db3 = new DBHandler3(this);
+
+        // Reading M3
+        Log.d("Reading: ", "Reading M3..");
+        List<M3> m3s = db3.getAllM3();
+
+        for (M3 m3 : m3s) {
+            String log = "Wijknummer: " + m3.getWijknummer() + " ,Naam: " + m3.getNaam() + " ,Trommels: " + m3.getTrommels() + " ,Diefstallen: " + m3.getDiefstallen();
+            // Writing shops to log
+            Log.d("M3: : ", log);
+        }
+
+        DBHandler4 db4 = new DBHandler4(this);
+
+        // Reading M4
+        Log.d("Reading: ", "Reading M4..");
+        List<M4> m4s = db4.getAllM4();
+
+        for (M4 m4 : m4s) {
+            String log = "MK_omschrijving: " + m4.getMK_omschrijving() + " ,Merk: " + m4.getMerk() + " ,Kleur: " + m4.getKleur();
+            // Writing shops to log
+            Log.d("M4: : ", log);
+        }
     }
 
     public void connect(View view) {
 
     }
 
-    public void display(String message){
+    public void display(String message) {
 
     }
 }
